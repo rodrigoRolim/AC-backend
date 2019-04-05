aadd89b2cffd95b7f03c1ca8abe5f1233682ba37
rodrigoRolim <rodrigorolimveras92@gmail.com>
2019-04-05 20:01:51 -0300

    integration test configured

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,9 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 
-app = express();
+const app = express();
 app.use(bodyParser);
 
 app.get('/', (req, res) => res.send("hello"));
 
-export default app;
+export default app
