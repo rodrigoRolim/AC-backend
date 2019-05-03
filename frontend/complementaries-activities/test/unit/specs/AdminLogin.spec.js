9760755d72ea8f53ecee1e93680e9d6802586966
rodrigoRolim <rodrigorolimveras92@gmail.com>
2019-05-03 20:53:56 -0300

    step red of adminLogin

---
+++ frontend/complementaries-activities/test/unit/specs/AdminLogin.spec.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+import { mount } from 'avoriaz'
+import AdminLogin from '@/components/AdminLogin'
+
+describe('Admin.vue', () => {
+  it('should acess account of admin', () => {
+    
+  })
+})
