7fb4332cd3ae830e210f6f06ef0ff4cb15c219be
rodrigoRolim <rodrigorolimveras92@gmail.com>
2019-05-04 17:09:25 -0300

    step red for unit test of admin user auth

---
+++ backend/test/unit/controllers/authentication.spec.js
@@ -0,0 +1,9 @@
+import UsersController from '../../../src/controllers/users'
+
+describe('Controllers: Users', () => {
+  describe('post() users', () => {
+    it('should return id session for admin user', () => {
+      const usersController = new UsersController()
+    })
+  })
+})
