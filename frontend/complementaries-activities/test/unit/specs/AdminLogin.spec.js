85a9871f5e050b27772a5adaa143fbcb3191838e
rodrigoRolim <rodrigorolimveras92@gmail.com>
2019-05-04 14:31:04 -0300

    init develop

--- frontend/complementaries-activities/test/unit/specs/AdminLogin.spec.js
+++ frontend/complementaries-activities/test/unit/specs/AdminLogin.spec.js
@@ -3,6 +3,19 @@ import { mount } from 'avoriaz'
 import AdminLogin from '@/components/AdminLogin'
 
 describe('Admin.vue', () => {
+  it('receive promisse', done => {
+    const expectedResponse = [{
+      token: 'token-01',
+      msg: 'success'
+    }]
+    const adminLogin = mount(AdminLogin)
+    adminLogin.vm.login = function () {
+     return expectedResponse
+    }
+    adminLogin.vm.login()
+    expect(adminLogin.vm.login()).to.equal(expectedResponse)
+    done()
+  })
   it('should acess account of admin', () => {
     
   })
