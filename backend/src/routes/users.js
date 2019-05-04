c2b7e64781b5814d8cdb04a64fed003328839e96
rodrigoRolim <rodrigorolimveras92@gmail.com>
2019-05-04 20:42:02 -0300

    integartion model and controller

--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -1,8 +1,9 @@
 import express from 'express'
 import UsersController from '../controllers/users'
+import User from '../models/users'
 
 const router = express.Router()
-const usersController = new UsersController()
+const usersController = new UsersController(User)
 
 router.post('/admin/login', (req, res) => usersController.post(req, res))
 
