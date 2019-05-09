9ae546250597f0312188b50abeaafcdfef192796
rodrigoRolim <rodrigorolimveras92@gmail.com>
2019-05-08 22:26:21 -0300

    imported ac-card-group component

--- frontend/complementaries-activities/src/components/AcRightbar.vue
+++ frontend/complementaries-activities/src/components/AcRightbar.vue
@@ -1,19 +1,77 @@
 <template>
   <div id="ac-rightbar">
-    <span>sidebar</span>
+    <ac-card-group class="card" v-for="group in groupList" :key="group.id" :group="group"></ac-card-group>
   </div>
 </template>
 
 <script>
+import AcCardGroup from './AcCardGroup'
 export default {
+  name: 'AcRightbar',
+  components: { AcCardGroup },
+  data () {
+    return {
+      groupList: [
+        {
+          name: 'grupo 1',
+          items: [
+            {
+              name: 'item 1',
+              id: 0
+            }
+          ]
+        },
+         {
+          name: 'grupo 2',
+          items: [
+            {
+              name: 'item 2',
+              id: 1
+            }
+          ]
+        },
+        {
+          name: 'grupo 2',
+          items: [
+            {
+              name: 'item 2',
+              id: 1
+            }
+          ]
+        }
+      ]
+    }
+  }
 }
 </script>
 
 <style scoped>
 #ac-rightbar {
   display: flex;
+  flex-direction: column;
   border-left: 1px solid rgba(0, 0, 0, 0.2);
-  height: 80vh;
-  width: 20%;
+  max-height: 81vh;
+  width: 28%;
+  overflow-y: auto;
+}
+
+/* width */
+::-webkit-scrollbar {
+  width: 10px;
+}
+
+/* Track */
+::-webkit-scrollbar-track {
+  background: #f1f1f1; 
+}
+ 
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background: rgba(0,0,0,0.1); 
+}
+
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+  background: #555; 
 }
 </style>
