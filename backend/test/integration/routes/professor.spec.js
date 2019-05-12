cf2dc32a5743a12323a6e4942073726e6f86f1d7
rodrigoRolim <rodrigorolimveras92@gmail.com>
2019-05-12 17:09:33 -0300

    integration test os save professor: step red

---
+++ backend/test/integration/routes/professor.spec.js
@@ -0,0 +1,52 @@
+import Professor from "../../../src/models/professor";
+
+describe('Routes: professor', () => {
+  let request
+
+  before(() => {
+    return setupApp()
+      .then(app => {
+        request = supertest(app)
+      })
+  })
+  const defautlId = '56cb91bdc3464f14678934ca'
+  const defaultProfessor = {
+    name: 'roger john',
+    email: 'roger@email.com',
+    graduation: '5cd87c81f9aedcbd1bb3a57d'
+  }
+
+  beforeEach(() => {
+    const professor = new Professor(defaultProfessor)
+    professor._id = '56cb91bdc3464f14678934ca'
+    return Professor.remove({})
+      .then(() => professor.save())
+  })
+
+  afterEach(() => Professor.remove({}))
+
+  describe('POST /admin/home', () => {
+    
+    context('when posting an professor', () => {
+      it('should return a new professor with status code 201', done => {
+        const customId = '56cb91bdc3464f14678934ba'
+        const newProfessor = Object.assign({}, {_id: customId, __v: 0}, defaultProfessor)
+        const expectedSavedProfessor = {
+          _id: customId,
+          name: 'roger john',
+          email: 'roger@email.com',
+          graduation: '5cd87c81f9aedcbd1bb3a57d'
+        }
+
+        request
+          .post('/professor/admin/home')
+          .send(newProfessor)
+          .end((err, res) => {
+            expect(res.statusCode).to.eql(201)
+            expect(res.body).to.eql(expectedSavedProfessor)
+            done(err)
+          })
+      })
+    })
+  })
+})
