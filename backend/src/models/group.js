eddbe11b3e8f4eaef1d0fe8997bd81fc20cc4de3
rodrigoRolim <rodrigorolimveras92@gmail.com>
2019-05-14 14:08:01 -0300

    created group model

--- backend/src/models/group.js
+++ backend/src/models/group.js
@@ -1,1 +1,12 @@
 // name or description, items
+import mongoose from 'mongoose'
+
+const schema = new mongoose.Schema({
+  name: String,
+  description: String,
+  items: [mongoose.model('Item')]
+})
+
+const Group = mongoose.model('Group', schema)
+
+export default Group
