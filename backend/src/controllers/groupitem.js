5f7de6542060f5714667c1be860a3850bd252905
rodrigoRolim <rodrigorolimveras92@gmail.com>
2019-05-14 19:23:29 -0300

    fix catch

--- backend/src/controllers/groupitem.js
+++ backend/src/controllers/groupitem.js
@@ -12,7 +12,7 @@ class GroupItemsController {
     const item = req.body
     return this.Group.findByIdAndUpdate(id, { $push: { items: item }})
       .then((updatedGroup) => res.status(201).send(updatedGroup))
-      .cathc((err) => res.status(400).send(err.message))
+      .catch((err) => res.status(400).send(err.message))
   }
 }
 
