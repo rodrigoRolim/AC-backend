deb17453d99f04ce65ba720be539739419de66f4
rodrigoRolim <rodrigorolimveras92@gmail.com>
2019-05-15 15:43:09 -0300

    3001 to 3000

--- frontend/complementaries-activities/src/services/config.js
+++ frontend/complementaries-activities/src/services/config.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const http = axios.create({
-  baseURL: 'http://localhost:3001',
+  baseURL: 'http://localhost:3000',
   timeout: 10000,
   headers: {
     'Access-Control-Allow-Origin': '*',
