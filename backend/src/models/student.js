9f6539809c5fe106410d43d6acbd80ad4a4e7f02
rodrigoRolim <rodrigorolimveras92@gmail.com>
2019-05-19 18:14:53 -0300

    create model

--- backend/src/models/student.js
+++ backend/src/models/student.js
@@ -1,6 +1,9 @@
 // name, email, graduation, password, grupo1_score, grupo2_score, total_score =  g1 + g2 + g3
 import mongoose from 'mongoose'
+import Util from 'util'
+import bcrypt from 'bcrypt'
 
+const hashAsync = Util.promisify(bcrypt.hash)
 const schema = new mongoose.Schema({
   ra: {
     type: String,
@@ -23,12 +26,46 @@ const schema = new mongoose.Schema({
     type: String,
     required: true
   },
-  group_one: Number,
-  group_two: Number,
-  group_three: Number,
-  total: Number
+  group_one: {
+    type: Number,
+    default: 0
+  },
+  group_two: { 
+    type: Number,
+    default: 0
+  },
+  group_three: { 
+    type: Number,
+    default: 0 
+  },
+  total: { 
+    type: Number,
+    default: 0
+  }
 })
-
+schema.set('toJSON', {
+  transform: (doc, ret, options) => ({
+    _id: ret._id,
+    ra: ret.ra,
+    name: ret.name,
+    email: ret.email,
+    graduation: ret.graduation,
+    group_one: ret.group_one,
+    group_two: ret.group_two,
+    group_three: ret.group_three
+  })
+})
+schema.pre('save', function (next) {
+  if (!this.password || !this.isModified('password')) {
+    return next()
+  };
+  hashAsync(this.password, 10)
+    .then(hashedPassword => {
+      this.password = hashedPassword
+      next()
+    })
+    .catch(err => next(err))
+})
 const Student = mongoose.model('Students', schema)
 
 export default Student
