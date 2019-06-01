492ceb2d8d93c700b5148f9d03b062bf2508cf10
rodrigoRolim <rodrigorolimveras92@gmail.com>
2019-06-01 13:59:30 -0300

    add delete and getById services

--- frontend/complementaries-activities/src/services/Document.js
+++ frontend/complementaries-activities/src/services/Document.js
@@ -2,14 +2,23 @@ import { http } from './config'
 
 export default {
   save: (formData) => {
-    console.log(formData)
+ 
     return http.post('/document/add', formData)
   },
   getFile: (path) => {
-    console.log(path)
+
     return http.get(`/document/${path}`, { responseType: 'arraybuffer'})
   },
   readAll: () => {
+
     return http.get('/document/all')
+  },
+  delete: (path) => {
+
+    return http.delete(`/document/${path}`)
+  },
+  getById: (id) => {
+    
+    return http.get(`/document/${id}`)
   }
 }
