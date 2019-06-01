b14ab85858a51d28937ba3f222e793f38800723e
rodrigoRolim <rodrigorolimveras92@gmail.com>
2019-06-01 14:25:58 -0300

    dinamic comments

--- frontend/complementaries-activities/src/components/Comments.vue
+++ frontend/complementaries-activities/src/components/Comments.vue
@@ -19,11 +19,11 @@
           class="headline grey lighten-2"
           primary-title
         >
-          Comentários do professor
+          {{ title }}
         </v-card-title>
 
         <v-card-text>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          {{ message }}
         </v-card-text>
 
         <v-divider></v-divider>
@@ -45,6 +45,8 @@
 
 <script>
 export default {
+  name: 'commments',
+  props: ['title', 'message'],
   data () {
     return {
       dialog: false
