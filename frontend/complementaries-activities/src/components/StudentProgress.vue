346783338881dbd43375a460b19c7741c7641929
rodrigoRolim <rodrigorolimveras92@gmail.com>
2019-06-06 17:33:22 -0300

    refactory getSocoreboard

--- frontend/complementaries-activities/src/components/StudentProgress.vue
+++ frontend/complementaries-activities/src/components/StudentProgress.vue
@@ -2,24 +2,24 @@
   <v-card class="text-xs-center progress" >
    <v-toolbar-title class="grey--text text--darken-2">Progresso</v-toolbar-title>
     <v-progress-circular
-      v-if="scoreboard && !aproved"
-      v-for="(group, index) in groups"
-      v-bind:key="group._id"
+      v-if="!aprobation()"
+      v-for="(score, index) in this.$store.getters.getBoard"
+      v-bind:key="index"
       :rotate="360"
       :size="130"
       :width="15"
-      :value="scoreboard[index].score*100/group.scoreMax"
+      :value="score.percent"
       :color="getColor(index)"
     >
-      {{ scoreboard[index].score }} <small>pontos</small> <strong>{{group.name}}</strong>
+      {{ score.raw }} <small>pontos</small> <strong>{{score.group}}</strong>
     </v-progress-circular>
-    </v-progress-circular>
+ 
     <v-progress-circular
-      v-if="aproved"
+      v-if="aprobation()"
       :rotate="360"
       :size="130"
       :width="15"
-      :value="scoreboard.total"
+      :value="total*100/70"
       color="#00C853"
     >
       <v-icon large color="#FDD835" class="fa4">fa-trophy</v-icon>
@@ -36,10 +36,10 @@ export default {
   data () {
     return {
       interval: {},
-      aproved: false,
+      scoreboard: [],
+      total: 0,
       value: 0,
       groups: null,
-      scoreboard: null,
       pointed_color: 0,
       colors: ['teal', 'primary', 'red']
     }
@@ -54,45 +54,46 @@ export default {
       })
       .then(() => this.setScoreboard())
       .then(() => this.aprobation())
-      .then((aproved) => this.aproved = aproved)
+      .catch((err) => console.log(err))
+   /*
+      .then((aproved) => this.aproved = aproved) */
   },
   methods: {
     getColor (count) {
       return this.colors[count%3]
     },
     setScoreboard () {
-      
-      let totals_groups = []
-      let total = 0
 
-      for (let i = 0; i < this.groups.length; i++) {
-        totals_groups[i] = 0
-        for (let j = 0; j < this.documents.length; j++) {
-          if (this.documents[j].group == this.groups[i].name) {
-            totals_groups[i] += this.documents[j].score
-            total += this.documents[j].score
-          }
-        }
-      }
+      const scores = this.groups.map((group) => {
+        const scores_valides = this.documents.filter(doc => {
+            return (doc.evaluation !== 'reproved') && doc.group == group.name
+          })
 
-     const scores = totals_groups.map((item,i) => {
-        return { 
-          group: this.groups[i].name, 
-          score: item, 
-          aproved: item >= this.groups[i].scoreMin,
-          evaluation: this.documents[i].evaluation    
-        }
+        const score = scores_valides.reduce((acc, doc_ap) => {
+            return acc + doc_ap.score
+          }, 0)
+  
+        return { group: group.name, percent: score*100/group.scoreMin, raw: score, min: group.scoreMin }
       })
-     const score_validates = scores.filter((item) => item.evaluation !== "reproved")
-     console.log(score_validates)
-     this.scoreboard = Object.assign([], score_validates, { total: total, aproved: total >= 70 })
  
+      this.scoreboard = scores
+      console.log(scores)
+      this.$store.dispatch('set', scores)
     },
     aprobation () {
-
-      const aproveds = this.scoreboard.filter((score) => score.aproved && score.evaluation !== 'reproved')
-      console.log(aproveds)
-      return aproveds.length == this.groups.length && this.scoreboard.aproved
+      this.scoreboard = this.$store.getters.getBoard
+      const total = this.scoreboard.reduce((acc, groupScore) => {
+        return acc + groupScore.raw
+      }, 0)
+      console.log(total)
+      console.log(this.scoreboard)
+      const aproveds_groups = this.scoreboard.filter((item) => item.raw >= item.min)
+      console.log(aproveds_groups.length)
+      if (total >= 70 && aproveds_groups.length == this.groups.length) {
+        this.total = total
+        return true
+      }
+      return false
     }
   }
 }
