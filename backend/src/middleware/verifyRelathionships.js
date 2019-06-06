b9d24511adce41327c110a339df85160fe072460
rodrigoRolim <rodrigorolimveras92@gmail.com>
2019-06-06 17:28:15 -0300

    add function for verify if it has  realtionships between group and documents

--- backend/src/middleware/verifyRelathionships.js
+++ backend/src/middleware/verifyRelathionships.js
@@ -1,6 +1,8 @@
 import Graduation from '../models/graduation'
+import Document from '../models/document'
+
 export default {
-  isExistRelations: (req, res, next) => {
+  relationsGraduations: (req, res, next) => {
     try {
       const resp = Graduation.find({ department: req.params.id });
       console.log(resp.length);
@@ -14,6 +16,19 @@ export default {
     }
     catch (err) {
       next();
+    }
+  },
+  relationsGroupsDocuments: (req, res, next) => {
+    try {
+      const resp = Document.find({ id_group: req.params.id });
+      if (resp.length > 0) {
+        res.status(403).send('O grupo tem relações ativas com documentos')
+        next('false')
+      } else {
+        next()
+      }
+    } catch (err) {
+      next()
     }
   } 
 }
