f58bcab8c95036ab9eb0d74c60d8b364a84ad63d
rodrigoRolim <rodrigorolimveras92@gmail.com>
2019-06-06 19:18:26 -0300

    create function for modify situation of student

--- backend/src/controllers/student.js
+++ backend/src/controllers/student.js
@@ -69,6 +69,11 @@ class StudentController {
       .catch((err) => res.status(400).send(err.message))
 
   }
+  setSituation (req, res) {
+    return this.Student.findOneAndUpdate({ _id: req.params.id }, { situation: req.body } )
+      .then(() => res.sendStatus(200))
+      .catch((err) => res.status(422).send(err))
+  }
   // criar um get all students de determinado departamento, mas somente os que já enviaram seus documentos
   // criar um atualizador do atributo already_student e dispará-lo ao professor pelo pusher: findOneAndUpdate
 }
