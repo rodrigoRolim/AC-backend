2e16a0771407a5ef9a584b44ca06c355d6f9135d
rodrigoRolim <rodrigorolimveras92@gmail.com>
2019-06-06 17:28:30 -0300

    rename function

--- backend/src/routes/department.js
+++ backend/src/routes/department.js
@@ -10,7 +10,7 @@ const departmentController = new DepartmentController(Department)
 router.post('/add', (req, res) => departmentController.create(req, res))
 router.get('/all', (req, res) => departmentController.readAll(req, res))
 router.put('/update/:id', (req, res) => departmentController.update(req, res))
-router.delete('/delete/:id', verifyRelathionship.isExistRelations, 
+router.delete('/delete/:id', verifyRelathionship.relationsGraduations, 
   (req, res) => departmentController.delete(req, res))
 
 export default router
