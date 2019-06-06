bb16ad9e627c9392c5617541811e435c9f7b89c7
rodrigoRolim <rodrigorolimveras92@gmail.com>
2019-06-06 17:30:26 -0300

    implement feature of student approval

--- frontend/complementaries-activities/src/components/ProfessorProgress.vue
+++ frontend/complementaries-activities/src/components/ProfessorProgress.vue
@@ -2,6 +2,7 @@
   <v-card class="text-xs-center progress" >
    <v-toolbar-title class="grey--text text--darken-2">Progresso</v-toolbar-title>
     <v-progress-circular
+      v-if="!aprobation()"
       v-for="(score, index) in this.$store.getters.getBoard"
       v-bind:key="index"
       :rotate="360"
@@ -12,13 +13,13 @@
     >
       {{ score.raw }} <small>pontos</small> <strong>{{score.group}}</strong>
     </v-progress-circular>
-    </v-progress-circular>
+
     <v-progress-circular
-      v-if="aproved"
+      v-if="aprobation()"
       :rotate="360"
       :size="130"
       :width="15"
-      :value="scoreboard.total"
+      :value="total*100/70"
       color="#00C853"
     >
       <v-icon large color="#FDD835" class="fa4">fa-trophy</v-icon>
@@ -36,7 +37,7 @@ export default {
     return {
       interval: {},
       scoreboard: [],
-      aproved: false,
+      total: 0,
       value: 0,
       groups: null,
       pointed_color: 0,
@@ -52,8 +53,9 @@ export default {
         return
       })
       .then(() => this.setScoreboard())
+      .then(() => this.aprobation())
       .catch((err) => console.log(err))
-   /*.then(() => this.aprobation())
+   /*
       .then((aproved) => this.aproved = aproved) */
   },
   methods: {
@@ -66,21 +68,34 @@ export default {
         const doc_aproveds = this.documents.filter(doc => {
             return doc.evaluation == 'aproved' && doc.group == group.name
           })
+        console.log(doc_aproveds)
         const score = doc_aproveds.reduce((acc, doc_ap) => {
-            return acc + doc_app.score
+            return acc + doc_ap.score
           }, 0)
-        return { group: group.name, percent: score*group.scoreMin, raw: score, min: group.scoreMin }
+        console.log(score)
+        
+        return { group: group.name, percent: score*100/group.scoreMin, raw: score, min: group.scoreMin }
       })
  
       this.scoreboard = scores
+      console.log(scores)
       this.$store.dispatch('set', scores)
+    },
+    aprobation () {
+      this.scoreboard = this.$store.getters.getBoard
+      const total = this.scoreboard.reduce((acc, groupScore) => {
+        return acc + groupScore.raw
+      }, 0)
+      console.log(total)
+      console.log(this.scoreboard)
+      const aproveds_groups = this.scoreboard.filter((item) => item.raw >= item.min)
+      console.log(aproveds_groups.length)
+      if (total >= 70 && aproveds_groups.length == this.groups.length) {
+        this.total = total
+        return true
+      }
+      return false
     }
-   /*  aprobation () {
-
-      const aproveds = this.scoreboard.filter((score) => score.aproved && score.evaluation == 'aproved')
-      console.log(aproveds)
-      return aproveds.length == this.groups.length && this.scoreboard.aproved
-    } */
   }
 }
 </script>
