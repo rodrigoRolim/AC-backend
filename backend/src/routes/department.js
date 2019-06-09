077f70874814aa9b3a09f7c96b5cb49741587982
rodrigoRolim <rodrigorolimveras92@gmail.com>
2019-06-09 14:58:35 -0300

    add verify

--- backend/src/routes/department.js
+++ backend/src/routes/department.js
@@ -8,7 +8,7 @@ const router = express.Router()
 const departmentController = new DepartmentController(Department)
 
 router.post('/add', verify.verifyJWT, (req, res) => departmentController.create(req, res))
-router.get('/all', verify.verifyJWT,(req, res) => departmentController.readAll(req, res))
+router.get('/all', verify.verifyJWT, (req, res) => departmentController.readAll(req, res))
 router.put('/update/:id', verify.verifyJWT, (req, res) => departmentController.update(req, res))
 router.delete('/delete/:id', verify.verifyJWT, verifyRelathionship.relationsGraduations, (req, res) => departmentController.delete(req, res))
 
