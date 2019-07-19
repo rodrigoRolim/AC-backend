51ebfc39ab8a1b3a3ce937f8834d04be3950a9bf
rodrigoRolim <rodrigorolimveras92@gmail.com>
2019-07-19 20:03:07 -0300

    create function to authorization

---
+++ backend/src/services/auth.js
@@ -0,0 +1,20 @@
+class Auth {
+  constructor(compare, jwt) {
+    this.compare = compare
+    this.jwt = jwt
+  }
+  authorization(password, student) {
+    return this.compare(password, student.password)
+      .then((resp) => {
+        if (resp) {
+          return this.jwt({ id: student.id }, process.env.SECRET, {
+            expiresIn: 86400
+          })
+        }
+        throw new Error('No authorization')
+      })
+      //.catch((err) => err.message)
+  }
+}
+
+export default Auth
