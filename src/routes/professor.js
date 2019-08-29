30ec0aeff1bfb7b698fc6211cc41e12077717961
rodrigoRolim <rodrigorolimveras92@gmail.com>
2019-08-29 15:01:21 -0300

    use authorization service

--- src/routes/professor.js
+++ src/routes/professor.js
@@ -5,11 +5,12 @@ import Professor from '../models/professor'
 import Util from 'util'
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
+import Authorization from '../services/auth'
 
 const compare = Util.promisify(bcrypt.compare)
 const router = express.Router()
-
-const professorController = new ProfessorController(Professor, jwt, compare)
+const authotization = new Authorization(jwt, compare)
+const professorController = new ProfessorController(Professor, authotization)
 
 router.post('/add', (req, res) => professorController.create(req, res))
 router.post('/login', (req, res) => professorController.login(req, res))
